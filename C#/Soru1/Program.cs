e79f09b06a75e73a413cb369b92643a242ad2fbc
Emirhan duzgun <hkemirhan@gmail.com>
2024-02-12 23:24:02 +0300

    Soru1

--- C#/Soru1/Program.cs
+++ C#/Soru1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
@@ -6,7 +7,50 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ArrayList liste = new ArrayList();
+            ArrayList listeasal = new ArrayList();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("{0}. sayıyı giriniz : ", i+1);
+                int sayi = int.Parse(Console.ReadLine());
+
+                if (sayi==1)
+                    liste.Add(sayi);
+                else if (sayi==2)
+                    listeasal.Add(sayi);    
+                
+
+            for (int n = 2; n < sayi-1; n++)
+            {
+                if (sayi % n == 0){
+                    
+                    liste.Add(sayi);
+                    break;}
+                else
+                {
+                    listeasal.Add(sayi);
+                    break;
+                }
+            }
+
+            }
+            
+            Console.WriteLine("ASAL SAYILAR");
+            foreach (var assa in listeasal)
+            {
+                
+                Console.WriteLine(assa);
+            }
+
+            Console.WriteLine("ASAL OLMAYAN SAYILAR");
+            foreach (var notasa in liste)
+            {
+                
+                Console.WriteLine(notasa);
+            }
         }
+
+        
     }
 }
