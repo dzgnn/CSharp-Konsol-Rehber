d0bc43e674df1202d69d5b732b1f7765f15f29a3
Emirhan duzgun <hkemirhan@gmail.com>
2024-02-13 14:19:30 +0300

    Basic Classes

---
+++ C#/classes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calisan calisan1 = new Calisan();
+            calisan1.adi = "Emirhan";
+            calisan1.soyadi = "Düzgün";
+            calisan1.no = "1234123";
+            
+            calisan1.CalisanBilgileri();
+
+        }
+    }
+
+    public class Calisan
+    {
+        public string adi;
+        public string soyadi;
+        public string no;
+        public void CalisanBilgileri(){
+
+            Console.WriteLine(adi);
+            Console.WriteLine(soyadi);
+            Console.WriteLine(no);
+        }        
+
+
+    }
+}
