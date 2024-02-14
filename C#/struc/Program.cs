eac2a7fe05ec6ab9b1f6f48d72ac751068f6e171
Emirhan duzgun <hkemirhan@gmail.com>
2024-02-14 16:30:29 +0300

    Struct

---
+++ C#/struc/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dikdortgen dikdortgen = new Dikdortgen();
+            dikdortgen.kisakenar = 3;
+            dikdortgen.uzunkenar = 5;
+
+            Console.WriteLine("alan : {0}", dikdortgen.AlanHesapla());
+
+            DikdortgenStruct dikdortgenStruct = new DikdortgenStruct();
+            dikdortgenStruct.kisakenar = 5;
+            dikdortgenStruct.uzunkenar = 10;
+            Console.WriteLine("alan : {0}", dikdortgenStruct.AlanHesapla());
+        }
+    }
+
+    class Dikdortgen
+    {
+
+        public int kisakenar;
+
+        public int uzunkenar;
+
+        public long AlanHesapla(){
+
+            return this.kisakenar * this.uzunkenar;
+        }
+    }
+
+
+    class DikdortgenStruct
+    {
+        public int kisakenar;
+
+        public int uzunkenar;
+
+
+        public long AlanHesapla(){
+
+
+            return this.kisakenar * this.uzunkenar; 
+        }
+
+    }
+}
